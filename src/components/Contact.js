8c8ed8262c649f0be57c4421a84898fa18c494c0
Maxime Papin <max.papin33@gmail.com>
2019-12-23 15:53:19 +0100

    contact transformed to class and status state added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import './Contact.css';
 
-function Contact(props) {
-    return (
-        <div className="Contact">
-            <div>
-                <img className="avatar" src={props.avatar} alt={props.name}/>
-            </div>
-            <div>
-                <h4 className="name">{props.name}</h4>
-                <div className="status">
-                    <div className={props.online ? 'status-online' : 'status-offline'}></div>
-                    <p className="status-text">{props.online ? 'online' : 'offline'}</p>
+class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            online: props.online
+        };
+    }
+    render() {
+        return (
+            <div className="Contact">
+                <div>
+                    <img className="avatar" src={this.props.avatar} alt={this.props.name}/>
+                </div>
+                <div>
+                    <h4 className="name">{this.props.name}</h4>
+                    <div className="status"
+                        onClick={event => {
+                        const newStatus = !this.state.online;
+                        this.setState({online: newStatus});
+                        }}
+                    >
+                        <div className={this.state.online ? 'status-online' : 'status-offline'}></div>
+                        <p className="status-text">{this.state.online ? 'online' : 'offline'}</p>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Contact;
